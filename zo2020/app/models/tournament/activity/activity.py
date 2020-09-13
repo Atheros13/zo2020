3b86ed9c7d81cd16f7bae5b7a1f4fa4ab733954e
Atheros <m.t.atheros@gmail.com>
2020-09-14 08:24:10 +1200

    13/09 Activity

--- zo2020/app/models/tournament/activity/activity.py
+++ zo2020/app/models/tournament/activity/activity.py
@@ -2,6 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class ActivityType(models.Model):
+
+    type = models.CharField(max_length=30, unique=True)
+    description = models.TextField(blank=True)
+
 class Activity(models.Model):
 
-    name = models.CharField(max_length=30, unique=True)
+    type = models.ForeignKey(ActivityType, on_delete=models.PROTECT, related_name='activities')
+    activity = models.CharField(max_length=30)
+    sub_activity = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
--- zo2020/app/models/tournament/tournament/tournament.py
+++ zo2020/app/models/tournament/tournament/tournament.py
@@ -7,26 +7,37 @@ from app.models.tournament.activity.activity import Activity
 
 class Tournament(models.Model):
 
+    ## CREATION STATS
     created_by = models.ForeignKey(Account, null=True, on_delete=models.SET_NULL, related_name='tournaments')
     created_on = models.DateField()
 
+    ## LINKED TOURNAMENTS
     parent_tournament = models.ForeignKey("Tournament", null=True, on_delete=models.SET_NULL, related_name='child_tournaments')
     # >> child_tournaments
 
-    activity = models.ForeignKey(Activity, null=True, on_delete=models.SET_NULL, related_name='tournaments')
+    ## ACTIVITY (can be plural)
+    activity = models.ManyToManyField(Activity, related_name='tournaments')
 
-    title = models.CharField(max_length=50)
-    #variation i.e. 2020 part of McEvedy Shield, 2020
+    ## TITLE
+    title = models.CharField(max_length=30)
+    title_qualifier = models.CharField(max_length=20) # ? could this be a model with attributes like type/value i.e. annual/2020
+    title_name = models.CharField(max_length=50)
+
     description = models.TextField()
 
+    ## ADMIN
     hubs = models.ManyToManyField(Hub, related_name='tournaments')
+    admins = models.ManyToManyField(Account, related_name='tournament_admins')
 
-    competitor_type = None # i.e. inter-Hub, inter-HubGroup, inter-Participant
-    competitor_type_name = models.CharField(max_length=20)
-
+    ## EVENTS (WHAT)
     # >> grades = TournamentGrade
     # >> contests = TournamentContest
     # >> events = TournamentEvents
+
+    ## COMPETITORS (WHO)
+    competitor_type = None # i.e. inter-Hub, inter-HubGroup, inter-Participant
+    competitor_type_name = models.CharField(max_length=20) # i.e. House
+
 
     def __str__(self):
 
